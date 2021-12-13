0203b49d7d905bc2b613f0dcd5210391757079c1
落小梅 <425605679@qq.com>
2021-12-13 20:27:39 +0800

    wip:workspace

--- src/api/http/cancelAbort.ts
+++ src/api/http/cancelAbort.ts
@@ -2,5 +2,5 @@ const controller = new AbortController() // 创建一个控制器
 const { signal } = controller
 
 signal.addEventListener('abort', () => {
-  console.log('aborted!')
+  // console.log('aborted!')
 })
